116d52eff497bf091fb9620b88b2acd8320e5eb2
SystemX <SystemX@DESKTOP-C8D5IOV>
2023-06-13 12:10:18 +0300

    Добавьте файлы проекта.

---
+++ калькулятор/Калькулятор1911.cpp
@@ -0,0 +1,18 @@
+#include "Калькулятор1911.h"
+
+double Калькулятор1911::Calculate(double x, char oper, double y)
+{
+    switch (oper)
+    {
+    case '+':
+        return x + y;
+    case '-':
+        return x - y;
+    case '*':
+        return x * y;
+    case '/':
+        return x / y;
+    default:
+        return 0.0;
+    }
+}
---
+++ калькулятор/Калькулятор1911.h
@@ -0,0 +1,7 @@
+#pragma once
+class Калькулятор1911
+{
+public:
+	double Calculate(double x, char oper, double y);
+};
+
